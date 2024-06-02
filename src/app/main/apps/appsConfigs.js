c68d0d0f1d8287b7ab161ebf255d267866be23c3
patilmanas04 <patilmanas1304@gmail.com>
2024-06-03 01:39:12 +0530

    Add the 'add supplier' button in the supplier page and complete the Shifts, Expenses and Events pages

--- src/app/main/apps/appsConfigs.js
+++ src/app/main/apps/appsConfigs.js
@@ -1,37 +1,39 @@
 // import AcademyAppConfig from './academy/AcademyAppConfig';
-// import CalendarAppConfig from './calendar/CalendarAppConfig';
+import CalendarAppConfig from './calendar/CalendarAppConfig';
 import MessengerAppConfig from './messenger/MessengerAppConfig';
 import ClientsAppConfig from './clients/ClientsAppConfig';
 import ECommerceAppConfig from './e-commerce/ECommerceAppConfig';
 // import FileManagerAppConfig from './file-manager/FileManagerAppConfig';
 // import HelpCenterAppConfig from './help-center/HelpCenterAppConfig';
-// import MailboxAppConfig from './mailbox/MailboxAppConfig';
-// import NotesAppConfig from './notes/NotesAppConfig';
+import MailboxAppConfig from './mailbox/MailboxAppConfig';
+import NotesAppConfig from './notes/NotesAppConfig';
 import ProfileAppConfig from './profile/profileAppConfig';
 // import ScrumboardAppConfig from './scrumboard/ScrumboardAppConfig';
 // import TasksAppConfig from './tasks/TasksAppConfig';
 import NotificationsAppConfig from './notifications/NotificationsAppConfig';
 import EmployeesAppConfig from "./employees/EmployeesAppConfig";
 import PhotoTrackingAppConfig from './photo tracking/PhotoTrackingAppConfig';
+import FinanceDashboardAppConfig from './finance/FinanceDashboardAppConfig';
 
 /**
  * The list of application configurations.
  */
 const appsConfigs = [
 	// AcademyAppConfig,
-	// CalendarAppConfig,
+	CalendarAppConfig,
 	MessengerAppConfig,
 	ClientsAppConfig,
 	ECommerceAppConfig,
 	// FileManagerAppConfig,
 	// HelpCenterAppConfig,
-	// MailboxAppConfig,
-	// NotesAppConfig,
+	MailboxAppConfig,
+	NotesAppConfig,
 	ProfileAppConfig,
 	// ScrumboardAppConfig,
 	// TasksAppConfig,
 	NotificationsAppConfig,
 	EmployeesAppConfig,
 	PhotoTrackingAppConfig,
+	FinanceDashboardAppConfig,
 ];
 export default appsConfigs;
